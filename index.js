ae6a528d1334bf7b55a3c0fa88e68cb7d372394c
Hypwreck <89440730+Hypwreck@users.noreply.github.com>
2021-08-31 11:25:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');      
+const Config = require('./config.json');    
+const client = new Discord.Client();
+const cooldowns = new Discord.Collection(); 
+const prefix = "."
+client.on('ready', () => {
+    console.log('Bot Online');
+});
+
+const { ready } = require("./handlers/ready.js")
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+
+["command"].forEach(handler => {
+  require(`./handlers/${handler}`)(client);
+});
+client.queue = new Map()
+process.on('unhandledRejection', console.error);
+
+  
+client.on("message", async message => {
+ 
+
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  if (!message.member)
+    message.member = message.guild.fetchMember(message);
+
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd.length === 0) return;
+
+  let command = client.commands.get(cmd);
+
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+  if (command) command.run(client, message, args);
+});
+
+
+
+client.login('ODcxODIyNTMwOTg0NDE1MjUy.YQg6AQ.dTP637xFTGxsXaJjWM-oBrm_AXY');
